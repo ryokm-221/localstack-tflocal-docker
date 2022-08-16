a882029665751386ab7a08d7d2e3e6268061f629
Ryo Kameda <85273741+ryokm-221@users.noreply.github.com>
2022-08-16 16:16:48 +0900

    Modified build script not to use `npm run build`.

--- sample-build.sh
+++ sample-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+cwd="$(pwd)"
 rm -f terraform/lambda_func.zip
 zip "terraform/lambda_func.zip" node_modules/ index.js package.json package-lock.json
-npm run build
+cd terraform && docker run --rm -it -v "$(pwd)":/app --net localstack_default tflocal:0.2 "apply" "-auto-approve" && cd $cwd
